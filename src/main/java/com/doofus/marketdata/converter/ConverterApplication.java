24dbcaf582879a93de79b7515dc513fdaac017ce
Kndarp Patel <kndarppatel@gmail.com>
2020-05-22 19:52:33 +0200

    Fix formatting

--- src/main/java/com/doofus/marketdata/converter/ConverterApplication.java
+++ src/main/java/com/doofus/marketdata/converter/ConverterApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConverterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConverterApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ConverterApplication.class, args);
+  }
 }
