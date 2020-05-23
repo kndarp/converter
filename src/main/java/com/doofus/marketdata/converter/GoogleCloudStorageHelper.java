0b83e430b24060e4cf5abf2836f95f6fe909984e
Kndarp Patel <kndarppatel@gmail.com>
2020-05-23 21:23:11 +0200

    Add google cloud dependencies

---
+++ src/main/java/com/doofus/marketdata/converter/GoogleCloudStorageHelper.java
@@ -0,0 +1,10 @@
+package com.doofus.marketdata.converter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GoogleCloudStorageHelper {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GoogleCloudStorageHelper.class);
+
+}
